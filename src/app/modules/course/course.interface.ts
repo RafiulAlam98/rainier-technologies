740e26f631c9b307ec4712af228e45dd1ef3e93d
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-12-19 12:36:36 +0600

    added course schema

---
+++ src/app/modules/course/course.interface.ts
@@ -0,0 +1,18 @@
+import { Model } from 'mongoose'
+
+export type ICourses = {
+  name: string
+  description: string
+  price: number
+  duration: string
+  level: string
+  topics: string[]
+  schedule: {
+    startDate: string
+    endDate: string
+    classDays: string[]
+    classTime: string
+  }
+}
+
+export type CourseModel = Model<ICourses>
