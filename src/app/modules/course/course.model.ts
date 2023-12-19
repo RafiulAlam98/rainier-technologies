9b43db0d6d9f721a042a7be81d3c4fc7ed5de4cb
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-12-19 12:43:26 +0600

    added course model

---
+++ src/app/modules/course/course.model.ts
@@ -0,0 +1,58 @@
+import { Schema, model } from 'mongoose'
+import { CourseModel, ICourses } from './course.interface'
+
+const CourseSchema = new Schema<ICourses>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    duration: {
+      type: String,
+      required: true,
+    },
+    level: {
+      type: String,
+      required: true,
+    },
+    topics: {
+      type: [String],
+      required: true,
+    },
+    schedule: {
+      startDate: {
+        type: String,
+        required: false,
+      },
+      endDate: {
+        type: String,
+        required: false,
+      },
+      classDays: {
+        type: [String],
+        required: false,
+      },
+      classTime: {
+        type: String,
+        required: false,
+      },
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  },
+)
+
+export const courses = model<ICourses, CourseModel>('courses', CourseSchema)
